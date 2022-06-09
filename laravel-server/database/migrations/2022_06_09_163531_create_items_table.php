300ddf5a8c697b8491a83d8ac0506badd3bb1f38
AliOlliek <olliek95@gmail.com>
2022-06-09 19:47:53 +0300

    Created "Items" Table for Migration

---
+++ laravel-server/database/migrations/2022_06_09_163531_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->char('name');
+            $table->longText('description')->NULL;
+            $table->longText('image')->NULL;
+            $table->integer('price')->NULL;
+            $table->char('location')->NULL;
+            $table->boolean('in_stock')->NULL;
+            $table->integer('stock_quantity')->NULL;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
