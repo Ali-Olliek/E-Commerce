137160584134fa6befd5cc9c47c365722400f2a8
AliOlliek <olliek95@gmail.com>
2022-06-10 13:23:37 +0300

    Items Controller - Display All/Single/Category

--- laravel-server/app/Http/Controllers/ItemsController.php
+++ laravel-server/app/Http/Controllers/ItemsController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Models\User;
+use App\Models\Feedback;
 
 class ItemsController extends Controller
 {
-    //
+    public function displayAll(){
+        $items = Item::all();
+        return response()->json([
+            "status" => "Success",
+            "Items" => $items
+        ], 200);
+    }
+
+
+    public function displayItem($name){
+        $item = Item::where("name", "LIKE", "%$name%")->get();
+    
+        return response()->json([
+            "status" => "Success",
+            "Item" => $item
+        ], 200);
+    }
+
+    public function displayCat($category){
+        $item = Item::where("category","LIKE", "%$category%")->get();
+        
+        return response()->json([
+            "status" => "Success",
+            "Item" => $item
+        ], 200);
+    }
+
 }
