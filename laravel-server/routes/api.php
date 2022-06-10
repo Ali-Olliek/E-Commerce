ce2bdc87e4fcd72904668c5bec838c7fbafa4731
AliOlliek <olliek95@gmail.com>
2022-06-10 11:22:54 +0300

    Defined Basic API Routes

--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -2,27 +2,48 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JWTController;
+use App\Http\Controllers\JWTController; // Users Controller
+use App\Http\Controllers\AdminsController;
+use App\Http\Controllers\ItemsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'api'], function($router) {
-    Route::post('/register', [JWTController::class, 'register']);
-    Route::post('/login', [JWTController::class, 'login']);
-    Route::post('/logout', [JWTController::class, 'logout']);
-    Route::post('/refresh', [JWTController::class, 'refresh']);
-    Route::post('/profile', [JWTController::class, 'profile']);
+// User Routes
+// Users can Signup, Sign in, Sign out, refresh their JWT token, view profile, and write a feedback (Review and Like)
+Route::group(['prefix'=>'v1'], function(){
+    Route::group(['prefix'=>'User'], function(){
+        Route::group(['middleware' => 'api'], function($router) {
+            Route::POST('/register', [JWTController::class, 'register']);
+            Route::POST('/login', [JWTController::class, 'login']);
+            Route::POST('/logout', [JWTController::class, 'logout']);
+            Route::POST('/refresh', [JWTController::class, 'refresh']);
+            Route::POST('/profile', [JWTController::class, 'profile']);
+            Route::POST('/feedback', [JWTController::class, 'feedback']);
+        });
+    });
+    
+    // Admin Routes
+    // Admins can upload an item, edit existing item, display existing users (optional), and monitor reviews (optional).
+    Route::group(['prefix' => 'Admin'], function(){
+        Route::group(['middleware' => 'role.admin'], function(){
+            Route::POST("/AddItem", [AdminsController::class, "addItem"])->name("Add-item");
+            Route::POST("/EditItem/{id}", [AdminsController::class, "editItem"])->name("Edit-item");
+            Route::GET("/DisplayUsers", [AdminsController::class, "displayUsers"])->name("Display-users");
+            Route::GET("/MonitorReviews", [AdminsController::class, "monitorReviews"])->name("Monitor-reviews");
+        });
+    });
+
+    // Item Routes
+    // Items can be displayed as a whole and as a single item, and sorted by their category.
+    Route::group(['prefix' => 'Items'], function(){
+        Route::GET("/AllItems", [ItemsController::class, "displayAll"])->name("Display-all");
+        Route::GET("/Item/{name}", [ItemsController::class, "displayItem"])->name("Display-item");
+        Route::GET("/ItemsCat/{category}", [ItemsController::class, "displayCat"])->name("Display-cat");
+    });
+
+    // Utility Routes
+    Route::GET("/not-found", [UtilitiesController::class, "notFound"])->name("not-found");
 });
