6671ce7757e9afd998362cde401beacbd414d7c9
AliOlliek <olliek95@gmail.com>
2022-06-11 21:41:27 +0300

    Created Preliminary JS functions for Admin

--- Electron-Admin/main.js
+++ Electron-Admin/main.js
@@ -14,3 +14,18 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
+
+const addItem = document.getElementById("additem");
+const addcat = document.getElementById("addcat");
+const addCatPage = document.getElementById("category");
+const addItemPage = document.getElementById("createitem");
+
+
+window.onload = function () {
+  addItemPage.style.display = "block";
+  addCatPage.style.display = "none";
+};
+addcat.addEventListener("click", function () {
+  addItemPage.style.display = "none";
+  addCatPage.style.display = "block";
+});
