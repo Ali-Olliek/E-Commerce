38c46efefee664d014b4a85cdae56764838310a6
AliOlliek <olliek95@gmail.com>
2022-06-13 09:04:22 +0300

    Admin can add a Category

--- Electron-Admin/scripts/admin.js
+++ Electron-Admin/scripts/admin.js
@@ -56,9 +56,7 @@ add_item.addEventListener("click", function(event){
         'Content-Type': 'form-data'
       }
     }).then(function(response){
-      console.log(response)
-      if(response.data.status === 
-        "Success"){
+      if(response.data.status === "Success"){
           let parent = document.getElementById("createitem");
           let message = document.createElement("div");
           message.classList.add("message");
@@ -70,6 +68,43 @@ add_item.addEventListener("click", function(event){
           }, 2000);
       }else{
         let parent = document.getElementById("createitem");
+        let message = document.createElement("div");
+        message.classList.add("message");
+        message.classList.add("failed");
+        message.innerText = "Failed to Add Item";
+        parent.append(message);
+        setTimeout(() => {
+          message.style.display = "none";
+        }, 2000);
+      }
+    })
+  })
+
+
+  add_category.addEventListener("click", function(){
+    let category_name = document.getElementById("category_name").value;
+    let url = "http://localhost:8000/api/v1/Admin/AddCategory";
+    let data = new FormData();
+      data.append("name", category_name)
+    axios({
+      method: "POST",
+      data: data,
+      url: url,
+    }).then(function (response) {
+      if (response.data.status === "Success") {
+        console.log('succ')
+        let parent = document.getElementById("wrapper");
+        let message = document.createElement("div");
+        message.classList.add("message");
+        message.classList.add("success");
+        message.innerText = "Item Added Successfully";
+        parent.append(message);
+        setTimeout(() => {
+          message.style.display = "none";
+        }, 2000);
+      } else {
+        console.log('fail')
+        let parent = document.getElementById("wrapper");
         let message = document.createElement("div");
         message.classList.add("message");
         message.classList.add("failed");
